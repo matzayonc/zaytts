2b1fe8dacd12076aa830d0d3acceb6a3fac8d913
Zayonc <60236390+matzayonc@users.noreply.github.com>
2021-02-22 21:50:41 +0100

    gui at index

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, send_file, request
+from gtts import gTTS
+import os
+
+from markupsafe import escape
+app = Flask(__name__)
+static = os.getcwd()+'\\static\\'
+
+counter = 0
+
+
+def stringToAudioFile(line):
+    global counter
+    counter += 1
+    filename = static + 'tts' + str(counter) + '.mp3'
+    tts = gTTS(text=line, lang='en')
+    tts.save(filename)
+    return filename
+
+
+
+@app.route('/')
+def hello():
+    return "<form method='GET' action='/tts'><input type='text' name='t'><input type='submit'></form>"
+
+@app.route('/tts/<words>')
+def tts(words):
+    return send_file(stringToAudioFile(words))
+
+    
+@app.route('/tts')
+def ttsByArg():
+    text = request.args.get('t')
+    return send_file(stringToAudioFile(text))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
